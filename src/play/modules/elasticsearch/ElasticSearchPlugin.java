5a1655c454093f40889bca01093a6ca594c6fad4
Filip.Stefanak <filip.stefanak@gmail.com>
2012-09-04 17:38:48 +0100

    bugfix lookupModel

--- src/play/modules/elasticsearch/ElasticSearchPlugin.java
+++ src/play/modules/elasticsearch/ElasticSearchPlugin.java
@@ -22,6 +22,7 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -41,6 +42,7 @@ import play.PlayPlugin;
 import play.db.Model;
 import play.modules.elasticsearch.ElasticSearchIndexEvent.Type;
 import play.modules.elasticsearch.adapter.ElasticSearchAdapter;
+import play.modules.elasticsearch.annotations.ElasticSearchable;
 import play.modules.elasticsearch.mapping.MapperFactory;
 import play.modules.elasticsearch.mapping.MappingUtil;
 import play.modules.elasticsearch.mapping.ModelMapper;
@@ -332,7 +334,17 @@ public class ElasticSearchPlugin extends PlayPlugin {
 	 * @return Class of the Model
 	 */
 	public static Class<?> lookupModel(final String indexType) {
-		return modelLookup.get(indexType);
+		final Class<?> clazz = modelLookup.get(indexType);
+		if (clazz != null) { // we have not cached this model yet
+			return clazz;
+		}
+		final List<Class> searchableModels = Play.classloader.getAnnotatedClasses(ElasticSearchable.class);
+		for (final Class searchableModel : searchableModels) {
+			if (getMapper(searchableModel).getTypeName() == indexType) {
+				return searchableModel;
+			}
+		}
+		throw new IllegalArgumentException("Type name '" + indexType + "' is not searchable!");
 	}
 
 }
